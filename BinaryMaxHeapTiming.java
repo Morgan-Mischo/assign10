4cb741de04e418ea8ee7694e3009be5347ef34c2
Morgan-Mischo <46653585+Morgan-Mischo@users.noreply.github.com>
2019-04-10 18:47:01 -0600

    Update BinaryMaxHeapTiming.java

--- BinaryMaxHeapTiming.java
+++ BinaryMaxHeapTiming.java
@@ -1,6 +1,7 @@
 
 package assign10;
 
+import java.util.ArrayList;
 import java.util.Collections;
 
 public class BinaryMaxHeapTiming {
@@ -11,8 +12,7 @@ public class BinaryMaxHeapTiming {
 	public static<E> void main (String[] args) {
 		
 		experiment3();
-		//experiment5();
-		//experiment6();
+		experiment5and6 ();
 	}
 
 	/**
@@ -26,6 +26,17 @@ public class BinaryMaxHeapTiming {
 		for (int i = 1000; i < problemSize + 1; i+=1000)
 		{
 			System.out.println(i + "\t" + testAdd(i) + "\t" + testPeek(i) + "\t" + testExtractMax(i) + "\n");
+					
+		}
+	}
+	
+	private static void experiment5and6 ()
+	{
+		System.out.println("N" + "\t" + "Heap" + "\t" + "Sort");
+		
+		for (int i = 1000; i < problemSize + 1; i+=1000)
+		{
+			System.out.println(i + "\t" + experiment5(i) + "\t" + experiment6(i) + "\n");
 					
 		}
 	}
@@ -174,13 +185,95 @@ public class BinaryMaxHeapTiming {
 		
 	}
 
-	private static void experiment5() {
-		// TODO Auto-generated method stub
+	private static String experiment5(int i) {
+		ArrayList<Integer> myList = new ArrayList<Integer>(); 
+		long startTime, midpointTime, stopTime = System.nanoTime();
+
+		// First, spin computing stuff until one second has gone by.
+		// This allows this thread to stabilize.
+		startTime = System.nanoTime();
+		while (System.nanoTime() - startTime < 1000000000) { 
+			// empty block
+		}
+
+		// Now, run the test.
+		startTime = System.nanoTime();
+
+		for(int j = 0; j < TIMES_TO_LOOP; j++)
+		{
+			myList.add(j); 
+			for(int k = 0; k < i; k++)
+			{
+				FindKLargest.findKLargestHeap(myList, k); 
+			}
+		}
+
+		midpointTime = System.nanoTime();
+
+		// Run a loop to capture the cost of running the "timesToLoop" loop and
+		// generating a random ISBN.
+		
+		for(int j = 0; j < TIMES_TO_LOOP; j++)
+		{
+			myList.add(j); 
+			for(int k = 0; k < i; k++)
+			{
+			}
+		}
+		stopTime = System.nanoTime();
+
+		// Compute the time, subtract the cost of running the loop
+		// from the cost of running the loop and doing the lookups.
+		// Average it over the number of runs.
+		double averageTime = (double) ((midpointTime - startTime) - (stopTime - midpointTime))
+				/ (TIMES_TO_LOOP);
+		return averageTime + "";
 		
 	}
 	
-	private static void experiment6() {
-		// TODO Auto-generated method stub
+	private static String experiment6(int i) {
+		ArrayList<Integer> myList = new ArrayList<Integer>(); 
+		long startTime, midpointTime, stopTime = System.nanoTime();
+
+		// First, spin computing stuff until one second has gone by.
+		// This allows this thread to stabilize.
+		startTime = System.nanoTime();
+		while (System.nanoTime() - startTime < 1000000000) { 
+			// empty block
+		}
+
+		// Now, run the test.
+		startTime = System.nanoTime();
+
+		for(int j = 0; j < TIMES_TO_LOOP; j++)
+		{
+			myList.add(j); 
+			for(int k = 0; k < i; k++)
+			{
+				FindKLargest.findKLargestSort(myList, k); 
+			}
+		}
+
+		midpointTime = System.nanoTime();
+
+		// Run a loop to capture the cost of running the "timesToLoop" loop and
+		// generating a random ISBN.
+		
+		for(int j = 0; j < TIMES_TO_LOOP; j++)
+		{
+			myList.add(j); 
+			for(int k = 0; k < i; k++)
+			{
+			}
+		}
+		stopTime = System.nanoTime();
+
+		// Compute the time, subtract the cost of running the loop
+		// from the cost of running the loop and doing the lookups.
+		// Average it over the number of runs.
+		double averageTime = (double) ((midpointTime - startTime) - (stopTime - midpointTime))
+				/ (TIMES_TO_LOOP);
+		return averageTime + "";
 		
 	}
 }
